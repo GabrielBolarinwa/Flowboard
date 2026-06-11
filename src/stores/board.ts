cbdedac33b79333953d8706237cd0d1deef11b1c
Gabriel <gabibola955@gmail.com>
2026-06-11 19:53:54 +0100

    Created application board store

---
+++ src/stores/board.ts
@@ -0,0 +1,23 @@
+import type { Board } from "@/types";
+import { defineStore } from "pinia";
+import { ref, type Ref } from "vue";
+export const useBoardStore = defineStore("BoardStore", () => {
+  const boards: Ref<Board[] | undefined> = ref(undefined);
+  function addBoard(board: Board) {
+    boards.value = [];
+    boards.value.push(board);
+  }
+  function deleteBoard(boardId: string) {
+    boards.value = boards.value?.filter((board) => board.id !== boardId);
+  }
+  function editBoard(boardId: string, editedBoard: Board) {
+    const board = boards.value?.find((board) => board.id === boardId);
+    if (board) {
+      board.name = editedBoard.name;
+    }
+  }
+  function getBoard(boardId: string) {
+    return boards.value?.filter((board) => board.id === boardId);
+  }
+  return { addBoard, deleteBoard, editBoard, getBoard };
+});
