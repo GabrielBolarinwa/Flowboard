1975ac9d364f6d101da36acab429c8887ff46942
Gabriel <gabibola955@gmail.com>
2026-06-18 13:45:47 +0100

    Mapped out status tokens and texts

---
+++ src/constants/index.ts
@@ -0,0 +1,17 @@
+export const statusColorVariableMap = {
+  todo: "--todo",
+  "in-progress": "--inprogress",
+  "in-review": "--review",
+  done: "--done",
+  cancelled: "--cancelled",
+  blocked: "--blocked",
+};
+
+export const statusTextMap = {
+  todo: "Todo",
+  "in-progress": "In Progress",
+  "in-review": "On Review",
+  done: "Done",
+  cancelled: "Cancelled",
+  blocked: "Blocked",
+};
