1b5d7360c2b8b8232d11c8d4e4fee57ff17a07a1
Gabriel <gabibola955@gmail.com>
2026-06-21 00:08:35 +0100

    Setup title change for Home page

--- src/router.ts
+++ src/router.ts
@@ -2,11 +2,15 @@ import { createRouter, createWebHistory } from "vue-router";
 import Board from "./pages/Board.vue";
 import Home from "./pages/Home.vue";
 const routes = [
-  { path: "/", component: Home },
+  { path: "/", component: Home, meta: { title: "Home Page — Flowboard" } },
   { path: "/board/:boardId", component: Board },
 ];
 
 export const router = createRouter({
   history: createWebHistory(),
   routes,
+});
+
+router.afterEach((to) => {
+  document.title = (to.meta.title as string) ?? "Flowboard";
 });
