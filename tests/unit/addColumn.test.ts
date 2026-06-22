c35278d07ae2d9510a2a8d95628ccca417a3b78c
Gabriel <gabibola955@gmail.com>
2026-06-22 19:16:08 +0100

    Full testing suite for add column store action

---
+++ tests/unit/addColumn.test.ts
@@ -0,0 +1,78 @@
+import { createPinia, setActivePinia } from "pinia";
+import { beforeEach, describe, expect, it, vi } from "vitest";
+import { Column, ColumnFormValue } from "../../src/types";
+import { seedBasicBoard } from "../fixtures/boardFixtures";
+
+beforeEach(() => {
+  setActivePinia(createPinia());
+});
+
+vi.mock("nanoid", () => ({
+  nanoid: () => "column-id",
+}));
+
+const generateMockItems = (count: number) => {
+  const mockStoreObject: Record<string, Column> = {};
+  for (let i = 0; i < count; i++) {
+    const id = `seeded-id-${i}`;
+    mockStoreObject[id] = {
+      id: id,
+      name: `board-${i}`,
+      boardId: "board-1",
+      cardIds: [],
+      wipLimit: null,
+    };
+  }
+  return mockStoreObject;
+};
+
+describe("description", () => {
+  it("should add new column", () => {
+    const { columnStore } = seedBasicBoard();
+    const column: ColumnFormValue = {
+      name: "New Column",
+    };
+    columnStore.addColumn(column, "board-1");
+    expect(columnStore.columns["column-id"]).toBeDefined();
+  });
+  it("should have a valid boardId", () => {
+    const { columnStore, boardStore } = seedBasicBoard();
+    const column: ColumnFormValue = {
+      name: "New Column",
+    };
+    columnStore.addColumn(column, "board-1");
+    expect(
+      boardStore.boards[columnStore.columns["column-id"].boardId],
+    ).toBeDefined();
+  });
+  it("should append columnId to board column Id array", () => {
+    const { columnStore, boardStore } = seedBasicBoard();
+    const column: ColumnFormValue = {
+      name: "New Column",
+    };
+    columnStore.addColumn(column, "board-1");
+    expect(boardStore.boards["board-1"].columnIds).toContain("column-id");
+  });
+  it("should do nothing when board limit is reached", () => {
+    const { boardStore, columnStore } = seedBasicBoard();
+    columnStore.columns = generateMockItems(6);
+    boardStore.boards["board-1"].columnIds = Object.values(
+      columnStore.columns,
+    ).map((column) => column.boardId === "board-1" && column.id);
+    const columnsBefore = JSON.parse(JSON.stringify(columnStore.columns));
+    const column: ColumnFormValue = {
+      name: "New Column",
+    };
+    expect(columnStore.addColumn(column, "board-1")).toBeUndefined();
+    expect(columnStore.columns).toEqual(columnsBefore);
+  });
+  it('should do nothing if boardId is invalid', () => {
+    const {  columnStore } = seedBasicBoard();
+    const columnsBefore = JSON.parse(JSON.stringify(columnStore.columns));
+    const column: ColumnFormValue = {
+      name: "New Column",
+    };
+    expect(columnStore.addColumn(column, "not-real")).toBeUndefined();
+    expect(columnStore.columns).toEqual(columnsBefore);
+  })
+});
