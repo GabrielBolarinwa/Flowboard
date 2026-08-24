11c6bf6c87f377bef4c74c93ffa558133afa3c5b
Gabriel <gabibola955@gmail.com>
2026-08-24 22:54:47 +0100

    Fixed issue where wrong board route navigation doesn't display not found page

--- src/router.ts
+++ src/router.ts
@@ -1,10 +1,24 @@
-import { createRouter, createWebHistory } from "vue-router";
+import {
+  createRouter,
+  createWebHistory,
+  type RouteLocationNormalizedLoadedGeneric,
+} from "vue-router";
 import Board from "./pages/Board.vue";
 import Home from "./pages/Home.vue";
 import NotFound from "./pages/NotFound.vue";
+import { useBoardStore } from "./stores/board.ts";
 const routes = [
   { path: "/", component: Home, meta: { title: "Home Page — Flowboard" } },
-  { path: "/board/:boardId", component: Board },
+  {
+    path: "/board/:boardId",
+    component: Board,
+    beforeEnter: (to: RouteLocationNormalizedLoadedGeneric) => {
+      const { boards } = useBoardStore();
+      if (!boards[to.params.boardId as string]) {
+        return { path: "/404" };
+      }
+    },
+  },
   {
     path: "/:pathMatch(.*)",
     component: NotFound,
